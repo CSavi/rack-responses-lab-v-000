6cff80ac06f74036d203320ab41e030780067f20
Carrie Savinell <carrielane89@gmail.com>
2018-07-05 14:47:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,12 @@ class Application
     resp = Rack::Response.new
 
 
-    if Time.now < 12 
+    if Time.now < 12
       resp.write "Good Morning!"
 
     else
       resp.write "Good Afternoon!"
-
+    end
     resp.finish
   end
 end
